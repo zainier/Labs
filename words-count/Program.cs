8f9f3afe27211c1c0f99627caeb5c73dc360ea43
Artem Zainier <azainier@artelogic.net>
2025-10-10 09:16:53 +0300

    Add word count program

---
+++ words-count/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Number of words: 0");
+            return;
+        }
+
+        int wordCount = 0;
+        bool inWord = false;
+
+        foreach (char c in input)
+        {
+            if (!char.IsWhiteSpace(c))
+            {
+                if (!inWord)
+                {
+                    wordCount++;
+                    inWord = true;
+                }
+            }
+            else
+            {
+                inWord = false;
+            }
+        }
+
+        Console.WriteLine("Number of words: " + wordCount);
+    }
+}

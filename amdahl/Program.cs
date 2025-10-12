8c7ef0a480dba00136d9f9862953e800ea8c4bd3
Artem Zainier <azainier@artelogic.net>
2025-10-12 19:17:20 +0300

    Add Amdahl's Law implementation

---
+++ amdahl/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class AmdahlLaw
+{
+    public static void Main(string[] args)
+    {
+        for (int p = 5; p <= 100; p += 5)
+        {
+            Console.Write($"\t{p}%");
+        }
+        Console.WriteLine();
+
+        for (int n = 1; n <= 10; n++)
+        {
+            Console.Write($"{n}");
+
+            for (int p = 5; p <= 100; p += 5)
+            {
+                double pFrac = p / 100.0;
+                double speedup = 1 / ((1 - pFrac) + pFrac / n);
+                Console.Write($"\t{speedup.ToString("F2")}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

8aecdc29bcc59659d991f46762aa7392e668c4d0
Artem Zainier <azainier@artelogic.net>
2025-10-21 00:12:49 +0300

    Implement practice-2

---
+++ practice-2/Program.cs
@@ -0,0 +1,29 @@
+﻿class Car
+{
+    private string keyCode;
+
+    public string Model { get; set; }
+
+    public void SpeedUp()
+    {
+        Console.WriteLine("Speeding up");
+    }
+
+    public void SpeedDown()
+    {
+        Console.WriteLine("Slowing down");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car myCar = new Car();
+        myCar.Model = "Toyota";
+
+        Console.WriteLine($"Car model: {myCar.Model}");
+        myCar.SpeedUp();
+        myCar.SpeedDown();
+    }
+}

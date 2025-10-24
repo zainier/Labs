18762c9828f98633845f16b11f2d8d5c9434042f
Artem Zainier <azainier@artelogic.net>
2025-10-24 09:51:37 +0300

    Implement practice-6-7

---
+++ practice-6-7/Program.cs
@@ -0,0 +1,38 @@
+﻿class MathOperation
+{
+    public virtual float Calculate(float a, float b)
+    {
+        return 0;
+    }
+}
+
+class SumOperation : MathOperation
+{
+    public override float Calculate(float a, float b)
+    {
+        return a + b;
+    }
+}
+
+class MultiplyOperation : MathOperation
+{
+    public override float Calculate(float a, float b)
+    {
+        return a * b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathOperation sumOp = new SumOperation();
+        MathOperation mulOp = new MultiplyOperation();
+
+        float sumResult = sumOp.Calculate(5, 10);
+        float mulResult = mulOp.Calculate(5, 10);
+
+        Console.WriteLine($"Sum: {sumResult}");
+        Console.WriteLine($"Multiplication: {mulResult}");
+    }
+}

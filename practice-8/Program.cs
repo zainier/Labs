042467e58857ec2f5f1a089ab4514eaa875247fe
Artem Zainier <azainier@artelogic.net>
2025-10-24 10:09:05 +0300

    Implement practice-8

---
+++ practice-8/Program.cs
@@ -0,0 +1,25 @@
+﻿class IntValue
+{
+    public int Value { get; set; }
+
+    public IntValue(int value)
+    {
+        Value = value;
+    }
+
+    public static IntValue operator +(IntValue a, IntValue b)
+    {
+        return new IntValue(a.Value + b.Value);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IntValue a = new IntValue(5);
+        IntValue b = new IntValue(10);
+        IntValue c = a + b;
+        System.Console.WriteLine(c.Value);
+    }
+}

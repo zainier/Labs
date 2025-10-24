fe7950aeda4a33fd19d56096065eb7dda7c78060
Artem Zainier <azainier@artelogic.net>
2025-10-24 10:46:20 +0300

    Implement practice-9

---
+++ practice-9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class IntValue
+{
+    public int Value { get; set; }
+
+    public IntValue(int value)
+    {
+        Value = value;
+    }
+
+    public static bool operator >(IntValue a, IntValue b)
+    {
+        return a.Value > b.Value;
+    }
+
+    public static bool operator <(IntValue a, IntValue b)
+    {
+        return a.Value < b.Value;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IntValue a = new IntValue(5);
+        IntValue b = new IntValue(10);
+
+        Console.WriteLine(a > b ? "a is greater than b" : "b is greater than a");
+    }
+}

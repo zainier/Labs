30dd277ad40d4bd9e1530c10cd8ec5dd9c25e176
Artem Zainier <azainier@artelogic.net>
2025-10-26 17:04:21 +0200

    Implement practice-10-11

---
+++ practice-10-11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class IntValue
+{
+    public int Value { get; set; }
+
+    public IntValue(int value)
+    {
+        Value = value;
+    }
+
+    public static IntValue operator ++(IntValue intValue)
+    {
+        return new IntValue(intValue.Value + 1);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        IntValue num = new IntValue(10);
+        Console.WriteLine($"Initial value: {num.Value}");
+
+        IntValue newNum1 = ++num;
+        Console.WriteLine($"Value after prefix increment: {newNum1.Value}");
+        Console.WriteLine($"Original variable after prefix increment: {num.Value}");
+
+        Console.WriteLine();
+
+        IntValue newNum2 = num++;
+        Console.WriteLine($"Value after postfix increment: {newNum2.Value}");
+        Console.WriteLine($"Original variable after postfix increment: {num.Value}");
+    }
+}

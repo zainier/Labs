bbdec8a73cbe5d3e147526daecde0d351499d8cd
Artem Zainier <azainier@artelogic.net>
2025-10-26 17:41:37 +0200

    Implement practice-13

---
+++ practice-13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Expression
+{
+    public string Value { get; }
+
+    public Expression(string value)
+    {
+        Value = value;
+    }
+}
+
+class LongExpression : Expression
+{
+    public int Length { get; }
+    public LongExpression(string value) : base(value)
+    {
+        Length = value.Length;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LongExpression longExpr = new LongExpression("x + y - z");
+        Console.WriteLine($"Expression: {longExpr.Value}");
+        Console.WriteLine($"Length: {longExpr.Length}");
+    }
+}

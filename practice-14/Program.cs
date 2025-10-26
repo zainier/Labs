df394ff4cf44d85b0fb3ff58812c0c61a5fc418b
Artem Zainier <azainier@artelogic.net>
2025-10-26 18:00:32 +0200

    Implement practice-14

---
+++ practice-14/Program.cs
@@ -0,0 +1,37 @@
+﻿class Button
+{
+    public string Name { get; set; }
+
+    private int clickCount = 0;
+
+    public Button(string name)
+    {
+        Name = name;
+    }
+
+    public void Click()
+    {
+        clickCount++;
+    }
+
+    public static void ShowCounter(Button button)
+    {
+        Console.WriteLine($"Button \"{button.Name}\" was clicked {button.clickCount} time(s).");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Button ok = new Button("OK");
+        Button cancel = new Button("Cancel");
+
+        ok.Click();
+        ok.Click();
+        cancel.Click();
+
+        Button.ShowCounter(ok);
+        Button.ShowCounter(cancel);
+    }
+}

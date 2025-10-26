b42b71a40bb9fad32b332f1e46f36265c0c1859f
Artem Zainier <azainier@artelogic.net>
2025-10-26 18:32:25 +0200

    Implement practice-15

---
+++ practice-15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Math
+{
+    public static double ToPower(double number, int power)
+    {
+        return System.Math.Pow(number, power);
+    }
+
+    public static double ToPower(double number)
+    {
+        return System.Math.Pow(number, 2);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num = 3;
+
+        Console.WriteLine($"3 to the power of 3 = {Math.ToPower(num, 3)}");
+        Console.WriteLine($"Square of 3 = {Math.ToPower(num)}");
+    }
+}
+

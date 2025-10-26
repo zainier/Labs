f46f329cbe1b06e2d878fe476ee5be3e12dd8da6
Artem Zainier <azainier@artelogic.net>
2025-10-26 17:27:16 +0200

    Implement practice-12

---
+++ practice-12/Program.cs
@@ -0,0 +1,33 @@
+﻿class Animal
+{
+    public string Name { get; }
+
+    public string Says { get;}
+
+    public Animal(string name, string says)
+    {
+        Name = name;
+        Says = says;
+    }
+
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"{Name} says {Says}");
+    }
+}
+
+class Dog : Animal
+{
+    public Dog() : base("Dog", "Woof")
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Dog();
+        animal.PrintInfo();
+    }
+}

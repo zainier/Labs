2ae7052defd8c5edbaf6bb76d14cf882617e5ed6
Artem Zainier <azainier@artelogic.net>
2026-01-31 18:07:19 +0200

    Implement DSA lab-7 task-1

---
+++ DSA/lab-7/1/Program.cs
@@ -0,0 +1,40 @@
+﻿public class HashTable
+{
+    private string[] _table;
+
+    public HashTable(int size)
+    {
+        _table = new string[size];
+        for (int i = 0; i < size; i++)
+        {
+            _table[i] = null;
+        }
+    }
+
+    public bool Add(string value)
+    {
+        int index = GetHash(value);
+        if (_table[index] == null)
+        {
+            _table[index] = value;
+            return true;
+        }
+        return false;
+    }
+
+    public bool Contains(string value)
+    {
+        int index = GetHash(value);
+        return _table[index] == value;
+    }
+
+    private int GetHash(string value)
+    {
+        int hash = 0;
+        foreach (char c in value)
+        {
+            hash += c;
+        }
+        return hash % _table.Length;
+    }
+}

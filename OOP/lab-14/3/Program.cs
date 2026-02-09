eabe3adf1ee83db2ffb55729ada029397447e1ec
Artem Zainier <azainier@artelogic.net>
2026-02-09 20:35:25 +0200

    Implement lab-14 task-3

---
+++ OOP/lab-14/3/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Publisher
+{
+    public string Name { get; set; }
+
+    public Publisher(string name)
+    {
+        Name = name;
+    }
+}
+
+
+public class Book: ICloneable
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public Publisher Publisher { get; set; }
+
+    public Book(string title, string author, int year, Publisher publisher)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+        Publisher = publisher;
+    }
+
+    public object Clone()
+    {
+        return new Book(Title, Author, Year, new Publisher(Publisher.Name));
+    }
+}
